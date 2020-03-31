7589ebc412034b28cdff2da8e04e61fa8a69960d
cleamichel <62941132+cleamichel@users.noreply.github.com>
2020-03-31 14:48:23 +0200

    MTN PIONS NOIRS
    ce n'est que le début, la fonction n'est pas finit

---
+++ projets/MICHEL TRUCHET NDJALA/fonction_pions_noirs.py
@@ -0,0 +1,24 @@
+﻿import random
+
+ListeJoueur=[6,7,8,4]
+ListeAlea=[0,1,2,3]
+
+def Fonc_Noir():
+    Nnoir=0
+
+    if ListeJoueur[0] not in ListeAlea:
+        Nnoir= Nnoir + 1
+
+    if ListeJoueur[1] not in ListeAlea:
+        Nnoir= Nnoir + 1
+
+    if ListeJoueur[2] not in ListeAlea:
+        Nnoir= Nnoir + 1
+
+    if ListeJoueur[3] not in ListeAlea:
+        Nnoir= Nnoir + 1
+    return Nnoir
+
+z= Fonc_Noir()
+print(z)
+
