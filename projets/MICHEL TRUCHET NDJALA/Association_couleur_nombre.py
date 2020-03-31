374d7a9caf74ee999e84e147a4722373e7741639
Guilhem420 <62941920+Guilhem420@users.noreply.github.com>
2020-03-31 14:44:51 +0200

    MTN : Association_couleur_nombre

---
+++ projets/MICHEL TRUCHET NDJALA/Association_couleur_nombre.py
@@ -0,0 +1,20 @@
+﻿def couleur(x):
+    if x==1:
+        z="cyan"
+    elif x==2:
+        z="orange"
+    elif x==3:
+        z="blue"
+    elif x==4:
+        z="saddlebrown"
+    elif x==5:
+        z="yellow"
+    elif x==6:
+        z="green"
+    elif x==7:
+        z="purple"
+    else:
+        z="pink"
+    return z
+
+print(couleur(5))
