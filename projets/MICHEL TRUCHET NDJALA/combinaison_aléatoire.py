1ec17992cdf51886bd455fa09a2f2f96a29b92f3
cleamichel <62941132+cleamichel@users.noreply.github.com>
2020-03-31 14:41:36 +0200

    MTN COMBI ALEATOIRE

---
+++ projets/MICHEL TRUCHET NDJALA/combinaison_aléatoire.py
@@ -0,0 +1,22 @@
+﻿import random
+def combi_aleatoire():
+    ListeNbre = [1,2,3,4,5,6,7,8]
+    random.shuffle(ListeNbre)
+    Ordi1= ListeNbre[0]
+    Ordi2= ListeNbre[1]
+    Ordi3= ListeNbre[2]
+    Ordi4= ListeNbre[3]
+    ListeOrdi = [Ordi1,Ordi2,Ordi3,Ordi4]
+    return ListeOrdi
+
+ListeAlea=combi_aleatoire()
+
+print(ListeAlea)
+
+
+
+
+
+
+
+
