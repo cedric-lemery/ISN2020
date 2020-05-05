5e9367713b4c39c58a2a252711dc355c997b5bb3
cedric-lemery <32592390+cedric-lemery@users.noreply.github.com>
2020-05-05 15:24:35 +0200

    KFL : suggestion sur game.py

--- projets/KOPKE FAVIEN LERAY/game.py
+++ projets/KOPKE FAVIEN LERAY/game.py
@@ -1,6 +1,8 @@
 import pygame
 import os
 
+from enemies import *
+
 class Jeu:
     def __init__(self):
         self.width = 1100
@@ -10,7 +12,7 @@ class Jeu:
         self.tour = []
         self.vie = 10
         self.argent = 100
-        self.bg = pygame.image.load(os.path.join("assets", "bg.png"))
+        self.bg = pygame.image.load(os.path.join("assets", "bg.png"))  #  analogue à pygame.image.load('assets\bg.png')
         self.bg = pygame.transform.scale(self.bg, (self.width, self.height))
 
 
@@ -18,7 +20,9 @@ class Jeu:
         run = True
         clock = pygame.time.Clock()
         compteur=0
+        enemy1 = Enemy(10,10,5,5,1,1)
         while run:
+            enemy1.draw(g.win)
 ##            clock.tick(60)
             compteur+=1
             if(compteur==100):
@@ -35,3 +39,4 @@ class Jeu:
 
 g = Jeu()
 g.run()
+
