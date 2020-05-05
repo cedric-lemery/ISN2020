d1663e53f86dfc8792d6dcb815ee56bf8acfd117
Cédric Lémery <cedric.lemery@gmail.com>
2020-05-05 14:49:05 +0200

    KFL : fix game.py

--- projets/KOPKE FAVIEN LERAY/game.py
+++ projets/KOPKE FAVIEN LERAY/game.py
@@ -11,20 +11,23 @@ class Jeu:
         self.vie = 10
         self.argent = 100
         self.bg = pygame.image.load(os.path.join("assets", "bg.png"))
-        self.bg = pygame.transform.scale(self.bg, (self.width, self.height))   
+        self.bg = pygame.transform.scale(self.bg, (self.width, self.height))
 
 
     def run(self) :
         run = True
         clock = pygame.time.Clock()
+        compteur=0
         while run:
-            clock.tick(60)
-            for event in pygame.event.get() : 
+##            clock.tick(60)
+            compteur+=1
+            if(compteur==100):
+                run=False
+            for event in pygame.event.get() :
                 if event.type == pygame.QUIT:
                     run  = False
-            self.draw
-
-    pygame.quit()
+            self.draw()
+        pygame.quit()
 
     def draw(self):
         self.win.blit(self.bg, (0,0))
