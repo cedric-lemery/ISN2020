a030162424ecccddcb7464b16df8d202760b16d4
amaurylnt <63296043+amaurylnt@users.noreply.github.com>
2020-05-12 14:03:03 +0200

    Add files via upload
    Je ne comprend pas pourquoi sa ne marche pas

--- projets/CLEMENTE LYONNET GOUTTE/Projet final.py
+++ projets/CLEMENTE LYONNET GOUTTE/Projet final.py
@@ -8,7 +8,41 @@ def facile():
     Fenetre2.title('Niveau Facile')
     Fenetre2.geometry('500x500')
     Bouton_quitter2=Button(Fenetre2,text="Quitter", command=Fenetre2.destroy)
-    Bouton_quitter2.place(x=445,y=460)
+    Bouton_quitter2.place(x=450,y=475)
+
+    lettre1=''
+    mot='ETE'
+    def Quitter():
+        Fenetre2.destroy()
+    def ajout(lettre):
+        global lettre1
+        lettre1+=lettre
+        label['text']=lettre1
+    def E():
+        ajout('E')
+        Bouton_E.destroy()
+    def E1():
+        ajout('E')
+        Bouton_E1.destroy()
+    def T():
+        ajout('T')
+        Bouton_T.destroy()
+
+    photo=PhotoImage(file="108.png.png")
+    zone_dessin = Canvas(Fenetre2,width=300,height=300,bd=8,relief="ridge")
+    zone_dessin.create_image(250,250,image=photo)
+    zone_dessin.pack()
+
+    label=Label(Fenetre2,text='')
+    label.place(x=250,y=400)
+    Bouton_E=Button(Fenetre2,text='E',command=E)
+    Bouton_E.place(x=300,y=450)
+    Bouton_E1=Button(Fenetre2,text='E',command=E1)
+    Bouton_E1.place(x=200,y=450)
+    Bouton_T=Button(Fenetre2,text='T',command=T)
+    Bouton_T.place(x=100,y=450)
+    Bouton_Valid=Button(Fenetre2,text='Validé',command=valid)
+    Bouton_Valid.place(x=400,y=450)
 
 def moyen():
     Fenetre3=Tk()
