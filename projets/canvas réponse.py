8baa83027a22331853b583c580c044a96a9866ed
cleamichel <62941132+cleamichel@users.noreply.github.com>
2020-05-19 15:14:22 +0200

    MTN 2EME FONCTION COULEUR

---
+++ projets/canvas réponse.py
@@ -0,0 +1,8 @@
+﻿Mon_canvasReponses = Canvas(bg='black')
+
+Mon_canvasReponses.place(x=int(w*0.70),y=int(h*0.10),width=250,height=500)
+for loop in range (3):
+    y4=80
+    Mon_canvasReponses.create_rectangle(x2+i,y2,x2+i+x3,y2+y3,fill='red')
+    y2=y4
+
